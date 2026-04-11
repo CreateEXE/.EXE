8d1bcbe58c8388a8adb3488a1ac88aea5b60926a
CreateEXE <Creator.EXE@Outlook.com>
2026-04-10 17:10:04 -0700

    Create SystemBridge.kt

---
+++ core/SystemBridge.kt
@@ -0,0 +1,8 @@
+package com.onisong.error.core
+
+// Every AI must reference this interface so they can "talk"
+interface SystemBridge {
+    fun sendSubjective(input: String) // Hemisphere A (Actor)
+    fun receiveObjective(output: String) // Hemisphere B (Auditor)
+    fun updateUI(state: UIState) // The Visuals
+}
